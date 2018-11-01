2d1700401991bec4b62bbef4da5d9dd21d047838
Nick <nidoelger@gmail.com>
2018-11-01 11:32:09 -0500

    Update Trivia.py
    player class added

--- Trivia.py
+++ Trivia.py
@@ -1,7 +1,24 @@
 import os, sys
 import pygame
 from pygame.locals import *
+import unittest
 
+class testPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player = Player("1")
+
+    def testInits(self):
+        self.assertEqual(self.player.points, 0)
+        self.assertEqual(self.player.name, "1")
+
+
+class Player:
+    def __init__(self, name):
+        self.points = 0
+        self.name = name
+
+    def __str__(self):
+        return self.name
 
 class Question:
     def __init__(self, q, a):
