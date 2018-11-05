2e7a99b84afed2a28fbdc3d46f09b68fe14742b0
alyssapacleb <42876282+alyssapacleb@users.noreply.github.com>
2018-11-04 20:26:21 -0600

    Fix index out of range problem

--- question_reader.py
+++ question_reader.py
@@ -7,11 +7,11 @@ def readQuestion(category):
             if len(line) > 1:
                 question_lines = line.split(';')
                 question_lines = [x.strip() for x in question_lines]
-                if len(question_lines) == 3:
-                    new_question = (question_lines[0], question_lines[1])
+                if len(question_lines) == 4:
+                    new_question = (question_lines[0], question_lines[1], question_lines[2])
                     question_list.append(new_question)
-                elif len(question_lines) == 4:
-                    new_question = (question_lines[0], question_lines[1], ast.literal_eval(question_lines[2]))
+                elif len(question_lines) == 5:
+                    new_question = (question_lines[0], question_lines[1], ast.literal_eval(question_lines[2]) , question_lines[3])
                     question_list.append(new_question)
 
     return question_list
