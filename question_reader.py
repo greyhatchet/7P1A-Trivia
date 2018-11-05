d4196824ea7c31671b6445ed56e07112871af6f1
GTWillis1 <36413480+GTWillis1@users.noreply.github.com>
2018-11-04 22:04:08 -0600

    Updated to work with unit tests

--- question_reader.py
+++ question_reader.py
@@ -2,17 +2,23 @@ import ast
 
 def readQuestion(category):
     question_list = []
-    with open(str(category)+'_questions.txt','r') as file:
-        for line in file:
-            if len(line) > 1:
-                question_lines = line.split(';')
-                question_lines = [x.strip() for x in question_lines]
-                if len(question_lines) == 4:
-                    new_question = (question_lines[0], question_lines[1], question_lines[2])
-                    question_list.append(new_question)
-                elif len(question_lines) == 5:
-                    new_question = (question_lines[0], question_lines[1], ast.literal_eval(question_lines[2]) , question_lines[3])
-                    question_list.append(new_question)
+    try:
+        with open(str(category)+'_questions.txt','r') as file:
+            for line in file:
+                if len(line) > 1:
+                    question_lines = line.split(';')
+                    question_lines = [x.strip() for x in question_lines]
+                    if len(question_lines) == 4:
+                        new_question = (question_lines[0], question_lines[1], question_lines[2])
+                        #print('New?: ' + str(new_question))
+                        question_list.append(new_question)
+                    elif len(question_lines) == 5:
+                        new_question = (question_lines[0], question_lines[1], ast.literal_eval(question_lines[2]), question_lines[3])
+                        #print('New?: ' + str(new_question))
+                        question_list.append(new_question)
+
+    except(FileNotFoundError):
+        print('Question file not found! Returning empty list')
 
     return question_list
 
